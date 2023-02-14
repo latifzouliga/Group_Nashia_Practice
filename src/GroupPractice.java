7f3013b7533ec8091ce63e0cde8118053b5b6bf6
rado <radoslaw.pawlowski.00@gmail.com>
2023-02-14 12:46:11 -0500

    added tc06 changes

--- src/GroupPractice.java
+++ src/GroupPractice.java
@@ -2,7 +2,7 @@ public class GroupPractice {
 
     public static void main(String[] args) {
 
-        System.out.println("TC06 Rado");
+        System.out.println("TC006 Rado");
 
         System.out.println("Dilvin TC002");
 
